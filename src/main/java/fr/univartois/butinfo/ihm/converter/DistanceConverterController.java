a446979b3fb0b9b19326f110eb31363b392d8a1e
Ludovic Thérage <ludovic_therage@ens.univ-artois.fr>
2026-09-09 20:41:11 +0200

    Modification du nom de variable

--- src/main/java/fr/univartois/butinfo/ihm/converter/DistanceConverterController.java
+++ src/main/java/fr/univartois/butinfo/ihm/converter/DistanceConverterController.java
@@ -77,7 +77,7 @@ public class DistanceConverterController {
         try {
 
             double pieds = Double.parseDouble(piedsTextField.getText());
-            double metrs = pieds * pied_en_metres;
+            double metres = pieds * pied_en_metres;
 
 
             metreTextField.setHeaderText(String.format("%.2f", metres));
